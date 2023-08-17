d060acb03e6691633773a728b59efed41d66ece9
davidcoats <david@getsafetycone.com>
2023-08-17 13:13:20 -0600

    Initial commit.

---
+++ source/R5T.Z0062.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0062.Construction
+{
+	/// <summary>
+	/// Construction console project for the R5T.Z0062 library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.Z0062/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0062
+{
+	/// <summary>
+	/// Values catch-all, net6.0 (for of any and all types and usages).
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
